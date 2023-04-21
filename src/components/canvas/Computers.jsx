d7e57daf7f82972b1d00f41e9eba304feeee7e22
ErickDC <erickdiazc717@gmail.com>
2023-04-21 12:38:24 -0500

    computers component fixed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
@@ -11,8 +11,19 @@ const Computers = () => {
 		<mesh>
 			<hemisphereLight intensity={0.15} groundColor="black" />
 			<pointLight intensity={1} />
+			<spotLight
+				position={[-20, 50, 10]}
+				angle={0.12}
+				penumbra={1}
+				intensity={1}
+				castShadow
+				shadow-mapSize={1024}
+			/>
 			<primitive
 				object={computer.scene}
+				scale={0.75}
+				position={[0, -3.25, -1.5]}
+				rotation={[-0.01, -0.2, -0.1]}
 			/>
 		</mesh>
 	)
@@ -40,4 +51,4 @@ const ComputerCanvas = () => {
 	)
 }
 
-export default Computers
+export default ComputerCanvas
